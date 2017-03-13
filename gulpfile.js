2514cb43e568295bc53b51e12b9ab2bf298f0e9e
Kaz Matthews and grinil khanna <kaz+grinil@>
2017-03-13 10:47:17 -0700

    add jsBrowserify gulp task. runs in browser correctly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 // these variables are the names of the devDependencies
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 // (nameOfTask to call later, function to run when we tell gulp to run task)
-gulp.task('myTask', function(){
-  console.log('hello gulp');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
