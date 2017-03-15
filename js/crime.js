42086393d8f2cdf6577e7c7efb811177b9591d12
Kaz Matthews and Shruti Priya <kaz+shruti@>
2017-03-15 16:41:24 -0700

    change number of 911 calls to 50.

--- js/crime.js
+++ js/crime.js
@@ -10,7 +10,7 @@ Crime.prototype.getCrime = function(crimeType, displayFunction) {
 
     currentMapObject = new Map(47.612988, -122.333540);
 
-    for(var i = 0; i < 20; i++) {
+    for(var i = 0; i < 50; i++) {
       var latitude = response[i].incident_location.coordinates[1];
       var longitude = response[i].incident_location.coordinates[0];
       currentMapObject.placeMarker(latitude, longitude);
